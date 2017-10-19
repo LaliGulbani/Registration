f3b07f4ced64c52cc716f3165fa53e8790c8d426
Lali Gulbani <lalushka9711@gmail.com>
2017-10-19 15:30:59 +0300

    example App Engine Java Endpoints Module

---
+++ app/src/main/java/laligulbani/by/registration/MainActivity.java
@@ -0,0 +1,35 @@
+package laligulbani.by.registration;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button log;
+    EditText login, password;
+    TextView tvLog, tvPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        log = (Button)findViewById(R.id.logButton);
+        login = (EditText)findViewById(R.id.loginEditText);
+        password = (EditText)findViewById(R.id.passwordEditText);
+        tvLog = (TextView)findViewById(R.id.textViewLogin);
+        tvPassword = (TextView)findViewById(R.id.textViewPassword);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new EndpointsAsyncTask().execute(new Pair<Context, String>(MainActivity.this, "Lali"));
+            }
+        });
+    }
+}
